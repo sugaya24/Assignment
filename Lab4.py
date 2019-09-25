a99d88177210a4ad3d3259adc3d45544f137add5
Sugaya <sugaya324@gmail.com>
2019-09-25 15:59:16 -0700

    commit

--- Lab4.py
+++ Lab4.py
@@ -6,23 +6,39 @@ def guessing_game():
     # generate a random integer from 1 to 1000
     answer = random.randint(1, 1000)
     # your code goes here.
-    guessing = int(input('Enter your guess from 1 to 1000: '))
-    print(answer)
     min = 0
     max = 1000
     count = 0
-    while guessing != answer:
-        print(answer, guessing)
+    while True:
+        # print(answer)
+        while True:
+            try:
+                guessing = int(input('Enter your guess from ' +
+                                     str(min) + ' to ' + str(max) + ': '))
+                break
+            except ValueError as e:
+                print(f"Invalid input: {e}")
+                print("Invalid input. Please enter a number!")
+            except KeyboardInterrupt:
+                print("\nNo input taken.\n")
+                break
+            except ZeroDivisionError as e:
+                print(f"Invalid input: {e}")
+            except EOFError as e:
+                print(f"Invalid input: {e}")
+
         count += 1
-        print("Wrong! Guess count: " + str(count))
-        if guessing > answer:
-            max = guessing - 1
+        if guessing == answer:
+            print("You got it! The hidden number is " + str(answer))
+            print("It took you " + str(count) + " guess(es)!")
+            break
         else:
+            print('Wrong! Guess count: ' + str(count))
+
+        if guessing < answer and guessing >= min:
             min = guessing + 1
-        guessing = int(input('Again Enter your guess from ' +
-                             str(min) + ' to ' + str(max) + ': '))
-    else:
-        print("You got it! The hidden number is " + str(answer))
+        elif guessing > answer and guessing <= max:
+            max = guessing - 1
 
 
 # https://stackoverflow.com/questions/419163/what-does-if-name-main-do
